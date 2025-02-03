6fa030f24548420acf109b1145f86c2022440f56
emzm17 <dibya172000@yahoo.com>
2025-02-04 01:44:28 +0530

    feat: ui

---
+++ src/constants/languageOptions.js
@@ -0,0 +1,44 @@
+export const languageOptions = [
+  {
+    id: 52,
+    name: "C++ (GCC 7.4.0)",
+    label: "C++ (GCC 7.4.0)",
+    value: "cpp",
+  },
+  {
+    id: 76,
+    name: "C++ (Clang 7.0.1)",
+    label: "C++ (Clang 7.0.1)",
+    value: "cpp",
+  },
+  {
+    id: 48,
+    name: "C (GCC 7.4.0)",
+    label: "C (GCC 7.4.0)",
+    value: "c",
+  },
+  {
+    id: 49,
+    name: "C (GCC 8.3.0)",
+    label: "C (GCC 8.3.0)",
+    value: "c",
+  },
+  {
+    id: 53,
+    name: "C++ (GCC 8.3.0)",
+    label: "C++ (GCC 8.3.0)",
+    value: "cpp",
+  },
+  {
+    id: 50,
+    name: "C (GCC 9.2.0)",
+    label: "C (GCC 9.2.0)",
+    value: "c",
+  },
+  {
+    id: 54,
+    name: "C++ (GCC 9.2.0)",
+    label: "C++ (GCC 9.2.0)",
+    value: "cpp",
+  },
+];
